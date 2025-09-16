d3330eb3fa62d738a1a201453df9ea7c48edc25b
ganeshpatnayak88 <mpganes9@gmail.com>
2025-09-16 22:35:31 +0530

    day 39

---
+++ day 39/day 39.py
@@ -0,0 +1,118 @@
+
+# num=int(input('enter a num:'))
+# if num%2==0:
+#     print('even num')
+# else:
+#     print('odd')
+
+
+# num=25
+# if num%5==0 and num%10!=0:
+#     print('satisfy')
+# else:print('not satify')
+
+# k=4
+# l=7
+# if k>l:
+#     print(k)
+
+
+# k=4
+# l=7
+# if k<l:
+#     print(k)
+
+# inp=18
+# if inp%2==0 and inp%3==0 and inp%6==0:
+#     print('satisfy')
+
+
+# age=19
+# if age>=18:
+#     print("eligible")
+# else:
+#     print("not eligible")
+
+# ma=20
+# ph=38
+# ch=25
+# c=0
+# if ma>=35:
+#     c+=1
+# if ph>=35:
+#     c+=1
+# if ch>=35:
+#     c+=1
+# if c==1:
+#     print('pass')
+# else:
+#     print('fail')
+
+
+# ma=20
+# ph=38
+# ch=67
+# c=0
+# if ma>=35:
+#     c+=1
+# if ph>=35:
+#     c+=1
+# if ch>=35:
+#     c+=1
+# if c>=2:
+#     print('pass')
+# else:
+#     print('fail')    
+
+
+# k=7
+# b=4
+# c=9
+# if k<=b and k<=c:
+#     print(k)
+# elif b<=k and b<=c:
+#     print(b)
+# else:
+#     print(c)
+
+# a=7
+# b=4
+# c=9
+# if a>b and a>c:
+#     print(a)
+# elif b>a and b>c:
+#     print(b)
+# else:
+#     print(c)
+
+# num=16
+# sq=int(num**0.5)
+# if num==sq*sq:
+#     print('yes')
+# else:
+#     print('no')
+
+# m=17
+# cars=m//5
+# if m%5!=0:
+#     cars+=1
+# print(cars)
+
+
+# a=10
+# b=20
+# c=30
+# if a>b and a<c or a<b and a>c:
+#     print(a)
+# elif b>a and b<c or b<c and b>a:
+#     print(b)
+# elif c>a and c<b or c<a and c>b:
+#     print(c)
+    
+# y=2024
+# if y%4==0 and y%100!=0:
+#     print('leap year')
+# else:
+#     print("not leap year")
+
+'''***end***'''
