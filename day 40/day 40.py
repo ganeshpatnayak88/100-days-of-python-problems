7ee11962cec92bfadbf8163a014349cbbefc3e96
ganeshpatnayak88 <mpganes9@gmail.com>
2025-09-18 00:00:25 +0530

    day 40

---
+++ day 40/day 40.py
@@ -0,0 +1,256 @@
+# n=int(input('enter a num:'))
+# for i in range(1,n+1):
+#     print(i,end=' ')
+
+
+# n=int(input('enter a num: '))
+# m=int(input('enter a num: '))
+# for i in range(n,m+1):
+#     print(i,end=' ')
+
+
+# n=int(input('enter a num: '))
+# for i in range(n,0,-1):
+#     print(i,end=' ')
+
+
+# n=int(input('enter a num: '))
+# m=int(input('enter a num: '))
+# for i in range(n,m-1,-1):
+#     print(i)
+    
+# n=int(input('enter a num: '))
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
+
+# n=int(input('enter a num: '))
+# j=1
+# for i in range(1,n+1):
+#     j*=i
+# print(j)
+    
+# n=int(input("enter a num:"))
+# m=int(input("enter a num:"))
+# s=0
+# for i in range(n,m+1):
+#     s+=i
+# print(s)
+
+# n=int(input("enter a num:"))
+# m=int(input("enter a num:"))
+# s=1
+# for i in range(n,m+1):
+#     s*=i
+# print(s)
+
+# n=int(input("enter a num:"))
+# for i in range(1,n+1):
+#     if n%i==0:
+#         print(i)
+
+# num=int(input("enter a num:"))
+# c=0
+# for i in range(1,num+1):
+#     if num%i==0:
+#         c+=1
+# print(c)
+
+# num=int(input("enter a num:"))
+# c=0
+# for i in range(1,num+1):
+#     if num%i==0:
+#         c+=1
+# if c==2:
+#     print('prime')
+# else:
+#     print('not prime')
+
+
+# n=int(input("enter a num:"))
+# m=int(input("enter a num:"))
+# for i in range(n,m+1):
+#     if i%2==0:
+#         print(i,end=' ')
+
+# n=int(input("enter a num:"))
+# m=int(input("enter a num:"))
+# for i in range(n,m+1):
+#     if i%2!=0:
+#         print(i,end=' ')
+
+
+
+# n=int(input("enter a num:"))
+# m=int(input("enter a num:"))
+# ce=0
+# co=0
+# for i in range(n,m+1):
+#     if i%2==0:
+#         ce+=1
+#     else:
+#         co+=1
+# print(ce,co)
+
+# d='sai'
+# f=''
+# for i in d:
+#     f=i+f
+# print(f)
+
+
+# p='sai'
+# rev=''
+# for i in p:
+#     rev=i+rev
+# print(rev)
+# if p==rev:
+#     print("parilndom")
+# else:
+#     print("not parilndom")
+
+
+# num=123456
+# s=str(num)
+# c=0
+# for i in s:
+#     c+=int(i)
+# print(c)
+
+# num=123
+# s=str(num)
+# c=1
+# for i in s:
+#     c*=int(i)
+# print(c)
+
+# f=153
+# s=str(f)
+# b=0
+# for i in s:
+#     b+=int(i)**3
+# if b==s:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# n=1234
+# f=str(n)
+# d=''
+# for i in f:
+#     d=i+d
+# print(d)
+
+# n=121
+# s=str(n)
+# h=''
+# for i in s:
+#     h=i+h
+# if h==s:
+#     print("yes")
+# else:
+#     print('no')
+
+# d="saiganesh"
+# v='aeiou'
+# c=0
+# for i in d:
+#     if i in v:
+#         c+=1
+# print(c)
+
+# f='saiganesh'
+# v='aeiou'
+# c=0
+# for i in f:
+#     if i not in v:
+#         c+=1
+# print(c)
+
+
+# d='saiganesh'
+# v='aeiou'
+# vc=0
+# cc=0
+# for i in d:
+#     if i in v:
+#         vc+=1
+#     else:
+#         cc+=1
+# print(vc,cc)
+
+
+# n=28
+# c=0
+# for i in range(1,n):
+#     if n%i==0:
+#         c+=i
+# if c==n:
+#     print('yes')
+# else:
+#     print("no")
+
+
+# n=9
+# m=str(n**2)
+# c=0
+# for i in m:
+#     c+=int(i)
+# if c==n:
+#     print("yes")
+# else:
+#     print("no")
+
+# n=145
+# m=0
+# for i in str(n):
+#     f=1
+#     for j in range(1,int(i)+1):
+#         f=f*int(i)
+#     m=m+f
+# if m==n:
+#     print(n,"yes")
+# else:
+#     print(n,"no")
+
+# num=145
+# m=0
+# for i in str(num):
+#     fact=1
+#     for j in range(1,int(i)+1):
+#         fact=fact*j
+#         print(fact)
+#     m=m+fact
+# if m==num:
+#     print('yes',m)
+# else:
+#     print('no',m)
+
+# n=45
+# s=0
+# for i in str(n):
+#     s+=int(i)
+# print(s)
+# if n%s==0:
+#     print("yes")
+# else:
+#     print("no")
+
+# n=5
+# a,b=0,1
+# for i in range(n):
+#     print(a,end=" ")
+#     a,b=b,a+b
+
+# n=9
+# s=(n**2)
+# k=0
+# for i in str(n):
+#     k+=int(i)
+# if k==n:
+#     print("yes")
+# else:
+#     print("no")
+
