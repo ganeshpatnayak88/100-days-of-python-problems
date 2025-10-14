5b532261f5f22f740d0854ca75e71997f6686aa6
ganeshpatnayak88 <mpganes9@gmail.com>
2025-10-14 18:51:24 +0530

    dsa day 1

---
+++ dsa/day 1.py
@@ -0,0 +1,29 @@
+# a=[1,2,43,56,87,90]
+# for i in range(len(a)):
+#     for j in range(i,len(a)):
+#         if a[i]>a[j]:
+#             a[i],a[j]=a[j],a[i]
+#         else:
+#             a[i],a[j]=a[i],a[j]
+# print(a)
+
+
+# a=[1,2,43,56,87,90,7]
+# for i in range(len(a)):
+#     min=i
+#     for j in range(i+1,len(a)):
+#         if a[j]<a[min]:
+#             min=j
+#     temp=a[i]
+#     a[i]=a[min]
+#     a[min]=temp
+# print(a)
+
+
+
+
+
+
+
+
+
